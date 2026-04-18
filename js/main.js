d3b95d84cca6b0526403b6cad2a52ba3014d6936
school-crypto <andre.davidsson@elev.ga.ntig.se>
2026-04-18 23:34:52 +0200

    Inlämmning
    inlämmningsklar kod

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-let menuButton = document.querySelector(".menu-button");
+let menuButton = document.querySelector(".menu_button");
 menuButton.addEventListener("click", showMenu);
 
-let fadeClick = document.querySelector(".fade-layer");
+let fadeClick = document.querySelector(".fade_layer");
 fadeClick.addEventListener("click", showMenu);
 
 function showMenu(){
@@ -9,7 +9,7 @@ function showMenu(){
     let menu = document.querySelector("nav.menu");
     menu.classList.toggle("show");
 
-    let layer = document.querySelector(".fade-layer");
+    let layer = document.querySelector(".fade_layer");
     layer.classList.toggle("visible");
 
 }
